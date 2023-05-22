fec015192c3636840bfbd0b01076452e390fe5ba
vidur-simform <vidur@simformsolutions.com>
2023-05-22 19:29:55 +0530

    initialization of practice project

---
+++ src/users/users.controller.ts
@@ -0,0 +1,11 @@
+import { Controller,Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  
+  createUser(){
+
+  }
+}
