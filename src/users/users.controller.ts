b4268ac4871d852b4175586622574517f28bc912
vidur-simform <vidur@simformsolutions.com>
2023-05-26 19:40:26 +0530

    tmp

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller,Post,Get,Patch,Param,Query, Delete, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Body, Controller,Post,Get,Patch,Param,Query, Delete, UseInterceptors, ClassSerializerInterceptor, Session } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto} from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -13,12 +14,24 @@ export class UsersController {
   constructor(private readonly usersService: UsersService,private readonly authService: AuthService) {}
  
   @Post('signup')
-  signup(@Body() body:CreateUserDto){
-    return this.authService.signup(body.email,body.password);
+  async signup(@Body() body:CreateUserDto, @Session() session :any){
+    const user = await this.authService.signup(body.email,body.password);
+    session.userId = user.id;
+    return user;
   }
   @Post('signin')
-  signin(@Body() body:CreateUserDto){
-    return this.authService.signin(body.email,body.password);
+  async signin(@Body() body:CreateUserDto, @Session() session :any){
+    const user = await this.authService.signin(body.email,body.password);
+    session.userId = user.id;
+    return user;
+  }
+  @Post('signout')
+  signOut(@Session() session :any){
+    session.userId = null;
+  }
+  @Get('/whoami')
+  whoAmI(@CurrentUser() user:string){
+    return user;
   }
   @Get()
   findAllUsers(@Query('email') email:string){
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable ,NotFoundException} from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
@@ -6,31 +6,34 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>){}
-    create(email:string,password:string){
-         const user = this.repo.create({email,password});
-         return this.repo.save(user);
-    }
-    async find(email:string){
-        if(!email)
-            throw new BadRequestException('Please provide email.');
-        const user = await this.repo.find({where:{email:email}});
-        return user;
-    }
-    findOne(id:number){
-        return this.repo.findOneBy({id});
-    }
-    async update(id:number,attrs:Partial<User>){
-        const user = await this.repo.findOneBy({id});
-        if(!user){
-            throw new NotFoundException('User not found!');
-        }
-        Object.assign(user,attrs);
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password });
         return this.repo.save(user);
     }
-    async remove(id :number){
-        const user = await this.repo.findOneBy({id});
-        if(!user){
+    async find(email: string) {
+        if (!email)
+            throw new BadRequestException('Please provide email.');
+        const user = await this.repo.find({ where: { email: email } });
+        return user;
+    }
+    findOne(id: number) {
+        if (!id) {
+            return null;
+        }
+        return this.repo.findOneBy({ id });
+    }
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.repo.findOneBy({ id });
+        if (!user) {
+            throw new NotFoundException('User not found!');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+    async remove(id: number) {
+        const user = await this.repo.findOneBy({ id });
+        if (!user) {
             throw new NotFoundException('User not found!');
         }
         return this.repo.remove(user);
